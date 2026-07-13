0b750219dd0874fac28ad41efa393e549576d8f0
Guillermo Martin <guillemartinsan@gmail.com>
2026-07-13 14:34:15 +0100

    Switched init_optim for minmax

--- src/SCN/low_rank_LIF.py
+++ src/SCN/low_rank_LIF.py
@@ -175,13 +175,13 @@ class Low_rank_LIF:
         N = E.shape[0]
 
         A, S = _canon_symmetric(Q)
-        negdef = -1 if np.all(np.diag(S) < 0) else 1
         EAinv = E @ np.linalg.inv(A)
         Coup = np.diag(
-            1 - 2 * np.all(np.isclose(EAinv[:, np.diag(S) == 1], 0, atol=1e-7), axis=1)
+            1 - 2 * np.all(np.isclose(EAinv[:, np.diag(S) == -1], 0, atol=1e-7), axis=1)
         )
 
-        D = spike_scale * -negdef * np.linalg.inv(Q) @ E.T @ Coup
+        D = np.linalg.inv(Q) @ E.T @ Coup
+        D = spike_scale * D
 
         if Fseed is not None:
             np.random.seed(Fseed)
